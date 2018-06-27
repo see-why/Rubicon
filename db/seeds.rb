dbc92dd951fd2527daf7a1b3ea95231d9fd2debe
Douwe Maan <douwe@gitlab.com>
2018-06-27 12:39:04 +0200

    Add scaffolding for Event model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Event.create(title: "Birthday Party", date: Date.today + 6.months)
