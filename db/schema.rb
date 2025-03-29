b8ac9c081060705678c4b3ab79daa06d93a23885
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-03-28 23:40:33 -0400

    Update database schema to add users table and upgrade ActiveRecord version

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2018_07_17_143852) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_29_033717) do
   create_table "events", force: :cascade do |t|
     t.string "title"
-    t.date "date"
     t.datetime "end_time"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
     t.datetime "start_time"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
